08ffd58114e2641551f030c9f942ff4daeec300c
teamkweku <teamkweku@outlook.com>
2023-12-23 12:34:01 +0000

    setting up MySQL database

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+import os
 from flask import Flask, request
 from flask_smorest import Api
 from resources.shop import blueprint as ShopBlueprint
 from resources.product import blueprint as ProductBlueprint
 import uuid
-from db import shops, products
+from db import db
+import models
 
 app = Flask(__name__)
 
@@ -14,6 +16,12 @@ app.config["OPENAPI_VERSION"] = "3.0.3"
 app.config["OPENAPI_URL_PREFIX"] = "/"
 app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
 app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+
+# Database config
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL')
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
 
 api = Api(app)
 api.register_blueprint(ShopBlueprint)
